c4d75b5a0b0bb0f815b26852c2f00174a0918f09
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-22 20:17:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
-require([
+ require([
         "esri/Map",
         "esri/views/MapView",
-        "esri/layers/Layer"
-      ], function (Map, MapView, Layer) {
+        "esri/layers/FeatureLayer"
+      ], function (Map, MapView, FeatureLayer) {
         var map = new Map({
-          basemap: "topo-vector"
+          basemap: "hybrid"
         });
 
-        var view = new MapView({
+         var view = new MapView({
           map: map,
           container: "viewDiv",
           zoom: 5,
           center: [-104.99, 39.74]
-        });
-
-        var layer = Layer.fromPortalItem({
-          portalItem: {
-            id: "99fd67933e754a1181cc755146be21ca"
-          }
         })
-          .then(addLayer)
-          .catch(rejection);
-  
-  var layer = Layer.fromPortalItem({
-          portalItem: {
-            id: "ea534c7d48894b269722f410bc5f45ad"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
 
-        // Adds the layer to the map once it loads
-        function addLayer(layer) {
-          map.add(layer);
-        }
-        function rejection(error) {
-          console.log("Layer failed to load: ", error);
-        }
-      })
+        /********************
+         * Add feature layer
+         ********************/
+
+        // Carbon storage of trees in Warren Wilson College.
+        var featureLayer = new FeatureLayer({
+          url:"https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_States_Generalized/FeatureServer/0"});
+        map.add(featureLayer);
+   
+   
+   var featureLayer = new FeatureLayer({
+     url:"https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/USA_State_Capitals/FeatureServer/0"
+   })
+        
+
+ });
